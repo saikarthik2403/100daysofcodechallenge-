888f2e2f2dc91a0652dcbd220524636a1d695c5b
Sai karthik <111523220+saikarthik2403@users.noreply.github.com>
2023-03-18 22:10:53 +0530

    Create To Lower Case.java

---
+++ To Lower Case.java
@@ -0,0 +1,37 @@
+//To Lower Case
+//https://leetcode.com/problems/to-lower-case/description/
+/*
+Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+ 
+
+Example 1:
+
+Input: s = "Hello"
+Output: "hello"
+Example 2:
+
+Input: s = "here"
+Output: "here"
+Example 3:
+
+Input: s = "LOVELY"
+Output: "lovely"
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s consists of printable ASCII characters.
+*/
+
+
+
+
+
+
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}

1a24195beffdca854749e26f15cc979cf5e9c232
Sai karthik <111523220+saikarthik2403@users.noreply.github.com>
2023-04-21 20:27:23 +0530

    Create Prefix Suffix String.java

---
+++ Prefix Suffix String.java
@@ -0,0 +1,91 @@
+//Prefix Suffix String
+//https://practice.geeksforgeeks.org/problems/5be83263c7f2cb866c60b23b73bb38f88de2461c/1
+/*
+Given two Lists of strings s1 and s2, you have to count the number of strings in s2 which is either a suffix or prefix of at least one string of s1.
+
+Example 1:
+
+Input:
+s1 = ["cat", "catanddog", "lion"]
+s2 = ["cat", "dog", "rat"]
+Output: 
+2
+Explanation: 
+String "cat" of s2 is prefix of "catanddog"
+& string "dog" of s2 is suffix of "catanddog" 
+Example 2:
+
+Input: 
+s1 = ["jrjiml", "tchetn", "ucrhye", "ynayhy", 
+       "cuhffd", "cvgpoiu", "znyadv"]
+s2 = ["jr", "ml", "cvgpoi", "gpoiu", "wnmkmluc", 
+      "geheqe", "uglxagyl", "uyxdroj"] 
+Output: 
+4
+Explanation: 
+String "jr" of s2 is prefix of "jrjiml", 
+"ml" of s2 is suffix of "jrjiml", 
+"cvgpoi" of s2 is prefix of "cvgpoiu" &
+"gpoiu" of s2 is suffix of "cvgpoiu"
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function prefixSuffixString(), which takes 2 strings s1 and s2 as input and returns an integer value as the number of strings in s2 which is a prefix or suffix in s1.
+
+Expected Time Complexity: O(max(len(s1) , len(s2) ))
+Expected Space Complexity: O(1)
+
+Constraints:
+   1 <= s1,s2 < 5 * 10^3
+   5 <= len(s1[i]), len(s2[i]) < 25
+   */
+
+
+
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+import java.io.*;
+
+public class code1 
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t=Integer.parseInt(in.readLine());
+        while(t-->0){
+            String s1[]=in.readLine().trim().split(" ");
+            String s2[]=in.readLine().trim().split(" ");
+
+            Solution ob=new Solution();
+            int ans=ob.prefixSuffixString(s1,s2);
+            out.println(ans);
+
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution
+{
+    public int prefixSuffixString(String s1[],String s2[])
+    {
+        int count = 0;
+        for (String s : s2) {
+            for (String t : s1) {
+                if (t.startsWith(s) || t.endsWith(s)) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
+
+
+
+
+
